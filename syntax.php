1a675665aea7018941f25265882ed62b1c23cdd8
Andreas Gohr <andi@splitbrain.org>
2009-06-08 21:28:09 +0200

    syntax works now
    Ignore-this: 585d3beb8fd29b1c4d52d1d023682572
    
    darcs-hash:20090608192809-7ad00-0e99a83a68df4b02e0116fafe8dfec7a0148aa2b.gz

--- syntax.php
+++ syntax.php
@@ -55,27 +55,42 @@ class syntax_plugin_panoview extends DokuWiki_Syntax_Plugin {
     function handle($match, $state, $pos, &$handler){
         global $ID;
 
-        $options = array(
-            'width'     => 500,
-            'height'    => 250,
-            'align'     => 'center',
-
-
-            'image'         => 'zoom:greetsiel.jpg',
-            'imageWidth'    => 7672,
-            'imageHeight'   => 1624,
+        $data = array(
+            'width'         => 500,
+            'height'        => 250,
+            'align'         => 0,
             'initialZoom'   => 2,
-
             'tileBaseUri'   => DOKU_BASE.'lib/plugins/panoview/tiles.php',
             'tileSize'      => 256,
             'maxZoom'       => 10,
             'blankTile'     => DOKU_BASE.'lib/plugins/panoview/gfx/blank.gif',
             'loadingTile'   => DOKU_BASE.'lib/plugins/panoview/gfx/progress.gif',
-
         );
 
+        $match = substr($match,11,-2); //strip markup from start and end
 
-        return $options;
+        // alignment
+        $data['align'] = 0;
+        if(substr($match,0,1) == ' ') $data['align'] += 1;
+        if(substr($match,-1,1) == ' ') $data['align'] += 2;
+
+        // extract params
+        list($img,$params) = explode('?',$match,2);
+        $img = trim($img);
+
+        // resolving relatives
+        $data['image'] = resolve_id(getNS($ID),$img);
+
+        $file = mediaFN($data['image']);
+        list($data['imageWidth'],$data['imageHeight']) = @getimagesize($file);
+
+        // size
+        if(preg_match('/^(\d+)[xX](\d+)$/',$params,$match)){
+            $data['width']  = $match[1];
+            $data['height'] = $match[2];
+        }
+
+        return $data;
     }
 
     /**
@@ -87,21 +102,22 @@ class syntax_plugin_panoview extends DokuWiki_Syntax_Plugin {
         require_once(DOKU_INC.'inc/JSON.php');
         $json = new JSON();
 
-        $img = '<a href="'.ml($data['image']).'"><img src="'.ml($data['image'],array('w'=>$data['width'],'h'=>$data['height'])).'" width="'.$data['width'].'" height="'.$data['height'].'" alt="" /></a>';
+        $img = '<a href="'.ml($data['image']).'"><img src="'.
+                    ml($data['image'], array('w'=>$data['width'],'h'=>$data['height'])).'" width="'.
+                    $data['width'].'" height="'.$data['height'].'" alt="" /></a>';
 
 
         $R->doc .= '
-<div class="panoview_plugin" style="width: '.$data['width'].'px; height: '.$data['height'].'px;">
-  <div class="well"><!-- --></div>
-  <div class="surface">'.$img.'</div>
-  <p class="controls" style="display: none">
-    <span class="zoomIn" title="Zoom In">+</span>
-    <span class="zoomOut" title="Zoom Out">-</span>
-  </p>
-    <div class="options" style="display:none">'.hsc($json->encode($data)).'</div>
-</div>
-
-';
+            <div class="panoview_plugin" style="width: '.$data['width'].'px; height: '.$data['height'].'px;">
+              <div class="well"><!-- --></div>
+              <div class="surface">'.$img.'</div>
+              <p class="controls" style="display: none">
+                <span class="zoomIn" title="Zoom In">+</span>
+                <span class="zoomOut" title="Zoom Out">-</span>
+              </p>
+                <div class="options" style="display:none">'.hsc($json->encode($data)).'</div>
+            </div>
+        ';
 
 
         return true;
