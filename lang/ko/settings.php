d1e43757a0aed76c573c9d750c118602bf901726
Myeongjin <aranet100@gmail.com>
2013-12-18 15:25:02 +0900

    Add Korean translation

---
+++ lang/ko/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['nice'] = '"멋진" 바이너리로의 경로. 타일 생성하는 동안 CPU 부하를 낮추기 위해 ImageMagick의 "변환" 도구와 함께 사용됩니다.';
